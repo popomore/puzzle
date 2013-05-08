ae807efac3472a9b3c06abf58a2581f50150b184
afc163 <afc163@gmail.com>
2013-05-08 17:13:38 +0800

    upgrade code for spm2

--- tests/puzzle-spec.js
+++ tests/puzzle-spec.js
@@ -1,11 +1,15 @@
 define(function(require) {
 
     var puzzle = require('../src/puzzle');
+    var expect = require('expect');
 
     describe('puzzle', function() {
 
-        it('normal usage', function() {
-
+        it('puzzle is existed', function() {
+            expect(puzzle).to.be.ok();
+            expect(puzzle.init).to.be.ok();
+            expect(puzzle.push).to.be.ok();
+            expect(puzzle.notExistedFunction).to.not.be.ok();
         });
     });
 
